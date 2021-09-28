874eec797fe0bfef5f6ce74fcff327ed410523fc
Prateek Bhardwaj <prateekb@dpogroup.com>
2021-09-28 09:27:14 +0200

    Prateek test output thread commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "01e08696-60af-4704-87ee-a53bd085e0a3",
+    "feature": "Navigating to the saucelab website and adding the product to the cart",
+    "scenario": "Add the product to the cart",
+    "start": 1632436584005,
+    "group": 15,
+    "content": "",
+    "tags": "",
+    "end": 1632436591601,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 15,
+    "content": "Thread[PoolService-0,5,main]"
+  }
+]);
+});
